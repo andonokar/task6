b0848c33d9df5bf56a1b66cc313287b3360e0964
Anderson <anderson.lima@domrock.ai>
2024-04-23 10:18:21 -0300

    added gold processing

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,6 @@
 from src.bronze import Bronze
 from src.silver import Silver
+from src.gold import Gold
 from src.sparkInit import start_spark
 
 
@@ -7,6 +8,7 @@ def main():
     spark = start_spark()
     Bronze.process(spark)
     Silver.process(spark)
+    Gold.process(spark)
 
 
 if __name__ == "__main__":
